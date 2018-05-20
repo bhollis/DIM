777e7065f1e58b839cdbdf6b87d8167171c5f872
Ben Hollis <ben@benhollis.net>
2018-05-19 19:50:46 -0700

    Use browserslist-useragent

--- src/browsercheck.js
+++ src/browsercheck.js
@@ -1,47 +1,15 @@
 
-import browserslist from 'browserslist';
-import parser from 'ua-parser-js';
+import { matchesUA } from 'browserslist-useragent';
 
-// Adapted from 'is-browser-supported' npm package. Separate from index.js so it'll run even if that fails.
-// This is also intentionally written in es5 and not TypeScript because it should not use any new features.
-
-function getBrowserName(agent) {
-  if (agent.browser.name === 'Chrome' && agent.os.name === 'Android') {
-    return 'and_chr';
-  } else if (agent.browser.name === 'Firefox' && agent.os.name === 'Android') {
-    return 'and_ff';
-  } else if (agent.browser.name === 'Mobile Safari' || (agent.browser.name === 'Safari' && agent.os.name === 'iOS')) {
-    return 'ios_saf';
-  } else if (agent.browser.name === 'Chromium') {
-    return 'chrome';
-  } else if (agent.browser.name === 'Opera') {
-    return 'opera';
-  }
-  return agent.browser.name;
-}
-
-function getBrowserVersionFromUserAgent(agent) {
-  var browserName = getBrowserName(agent);
-  var version = (browserName === 'ios_saf' ? agent.os.version : (agent.browser.version || agent.os.version || '')).split('.');
-  while (version.length > 0) {
-    try {
-      return browserslist(browserName + ' ' + version.join('.'))[0];
-    } catch (e) {
-      // Ignore unknown browser query error
-    }
-    version.pop();
-  }
-  return 'unknown';
-}
-
-var agent = parser(navigator.userAgent);
-if (agent !== 'Vivaldi') { // Vivaldi users can just live on the edge
-  var browsersSupported = browserslist($BROWSERS);
-  var browser = getBrowserVersionFromUserAgent(agent);
-  var supported = browsersSupported.indexOf(browser) >= 0;
+if (navigator.userAgent.indexOf('Vivaldi') < 0) { // Vivaldi users can just live on the edge
+  var supported = matchesUA(navigator.userAgent, {
+    browsers: $BROWSERS,
+    ignoreMinor: true,
+    _allowHigherVersions: true
+  });
 
   if (!supported) {
-    console.warn('Browser ' + browser + ' is not supported by DIM. Supported browsers:', browsersSupported);
+    console.warn('Browser ' + browser + ' is not supported by DIM.');
     document.getElementById('browser-warning').className = '';
   }
 }
